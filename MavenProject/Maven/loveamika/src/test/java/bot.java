83c74756c6896667d7315f842c25bf20f3e199ca
Nelson George <neon4frndsss@gmail.com>
2020-08-08 20:02:26 +0100

    Code 08-08-20

--- MavenProject/Maven/loveamika/src/test/java/bot.java
+++ MavenProject/Maven/loveamika/src/test/java/bot.java
@@ -1,12 +1,16 @@
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JList.DropLocation;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class bot {
@@ -15,24 +19,33 @@ public class bot {
       WebDriverManager.chromedriver().setup();
       WebDriver driver = new ChromeDriver();
       WebDriverWait wait = new WebDriverWait(driver, 20);
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       driver.manage().window().maximize();
       driver.get("https://www.lazada.com.ph/");
       driver.findElement(By.cssSelector("input[id='q']")).sendKeys("N95 Mask" + Keys.ENTER);
-      String productName = driver.findElement(By.xpath("(//a[contains(.,'N95')])")).getText();
-      driver.findElement(By.xpath("(//div[@class='cRjKsc'])")).click();
+      String productName = driver.findElement(By.xpath("(//div[@class='c16H9d'])[2]")).getText();
+      driver.findElement(By.xpath("(//div[@class='cRjKsc'])[2]")).click();
       Assert.assertEquals(driver.findElement(By.cssSelector("span[class='pdp-mod-product-badge-title']")).getText(),
             productName);
+      /*
+       * while (!(driver.findElement(By.xpath(
+       * "//div[@class='next-number-picker-input-wrap']")).getText().equals("5"))) {
+       * driver.findElement(By.cssSelector("a[class*='handler-up']")).click(); } ;
+       */
+      // if(driver.findElement(By.xpath("//div[@class='content-block
+      // sfo']")).isDisplayed())
 
-      while (!(driver.findElement(By.xpath("//span/input[@type='text']")).getText().equals("5"))) {
-         driver.findElement(By.cssSelector("a[class*='handler-up']")).click();
-      }
-      ;
       driver.findElement(By.cssSelector("button[class*='pdp-button_theme_orange']")).click();
-      WebElement frameLogin=driver.findElement(By.xpath("//iframe[@class='login-iframe']"));
+      WebElement frameLogin = driver.findElement(By.xpath("//iframe[@class='login-iframe']"));
       driver.switchTo().frame(frameLogin);
-      driver.findElement(By.xpath("//input[contains(text(),'Please enter your Phone Number or Email')]")).sendKeys("Username@test.com");
+      driver.findElement(By.xpath("//div//input[@placeholder='Please enter your Phone Number or Email']"))
+            .sendKeys("Username@test.com");
       driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Open@12345");
-      driver.findElement(By.cssSelector("button[type='submit']")).click();
-      driver.quit();
+      WebElement dragElement = driver.findElement(By.cssSelector("span[id*='_n1z']"));
+      WebElement dropLocation = driver.findElement(By.cssSelector("div[id*='scale_text']"));
+      Actions action = new Actions(driver);
+      action.dragAndDrop(dragElement, dropLocation).build().perform();
+     
+      
    }
 }
--- PracticePage/Homepage.js
+++ PracticePage/Homepage.js
@@ -1,20 +1,35 @@
-describe("This is the automation of homepage",function(){
+describe("This is the automation of homepage", function () {
 
-it("This is to validate the form", function(){
+    function selectProduct(product) {
+        element.all(by.css("div[class='card-body']")).then(function (products) {
+            products.element(by.tagName("h4")).getText().then(function (productName) {
+                if (productName.toEqual(product)) {
+                    products.element(by.css("button[class*='btn-info']")).click();
+                }
+            })
+        })
+    }
 
-    browser.get("https://qaclickacademy.github.io/protocommerce/");
-    element(by.css("input[name='name']")).sendKeys("Student1");
-    element(by.css("input[name='email']")).sendKeys("student1@mail.com");
-    element(by.css("input[id='exampleInputPassword1']")).sendKeys("Password");
-    element(by.css("[id='exampleCheck1']")).click();
-    element(by.cssContainingText("select[id='exampleFormControlSelect1'] option","Female")).click();
-    element.all(by.name("inlineRadioOptions")).first().click();
-    element(by.css("input[type='submit']")).click();
-    expect(element(by.css("div[class*='alert-dismissible']")).getText()).toContain("Success! The Form has been submitted successfully!.")
-    element(by.css("input[name='name']")).sendKeys("S")
-    expect(element(by.css("[class*='alert-danger']")).getText()).toContain("Name should be at least 2 characters");
+    it("This is to validate the form", function () {
 
+        browser.get("https://qaclickacademy.github.io/protocommerce/");
+        element(by.css("input[name='name']")).sendKeys("Student1");
+        element(by.css("input[name='email']")).sendKeys("student1@mail.com");
+        element(by.css("input[id='exampleInputPassword1']")).sendKeys("Password");
+        element(by.css("[id='exampleCheck1']")).click();
+        element(by.cssContainingText("select[id='exampleFormControlSelect1'] option", "Female")).click();
+        element.all(by.name("inlineRadioOptions")).first().click();
+        element(by.css("input[type='submit']")).click();
+        expect(element(by.css("div[class*='alert-dismissible']")).getText()).toContain("Success! The Form has been submitted successfully!.")
+        element(by.css("input[name='name']")).sendKeys("S")
+        expect(element(by.css("[class*='alert-danger']")).getText()).toContain("Name should be at least 2 characters");
+        element(by.css("a[class='nav-link']")).click();
+        selectProduct("iphone X");
+        selectProduct("Nokia Edge");
+        element(by.css("a[class*='btn-primary']")).getText().then(function(checkoutValue){
+            
 
-})
+        })
+    })
 
 })
